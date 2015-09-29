7bc391f9d81ada62fc583ba36d595935ebb4135e
Antoan Srv <antoan.srv@gmail.com>
2015-09-29 15:54:19 +0300

    Added Directions
    Added one line of text

--- Homework/PrintSequence.cs
+++ Homework/PrintSequence.cs
@@ -1,8 +1,11 @@
+using System;
 
 class PrintSequence
 {
     static void Main()
     {
+        Console.WriteLine("Displayed is the sequence of numbers between 2 and 12");
+
         for (int i = 2; i < 12; i++)
         {
             if (i % 2 == 0)
